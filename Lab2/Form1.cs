06272d7d8ecc0ff465c7e7da156685ad020d3467
mateusz.siedlarz <mateusz.siedlarz@COLLATZ>
2018-03-01 15:05:13 +0100

    added few methods

--- Lab2/Form1.cs
+++ Lab2/Form1.cs
@@ -12,6 +12,7 @@ namespace Lab2
 {
     public partial class Form1 : Form
     {
+        string uzytkownik;
         public Form1()
         {
             InitializeComponent();
@@ -29,6 +30,28 @@ namespace Lab2
             errorProviderPolTekstowych.SetError(poleTekstowe, "");
             return true;
         }
+
+        private void CzyscFormularz()
+        {
+            textBoxImie.Clear();
+            textBoxNazwisko.Clear();
+        }
+
+        private bool DodajUzytkownikaDoListy(string uzytkownik)
+        {
+            if (listBox1.Items.Contains(uzytkownik))
+            {
+                //zaimplementować 
+                return false;
+            }
+            else
+            {
+                listBox1.Items.Add(uzytkownik);
+                CzyscFormularz();
+                textBoxImie.Focus();
+                return true;
+            }
+        }
         #endregion
 
         private void buttonDodaj_Click(object sender, EventArgs e)
@@ -36,7 +59,10 @@ namespace Lab2
             //Nie trzeba &&, lepiej to zrobić &
             if (CzyPoleWypelnione(textBoxImie) & CzyPoleWypelnione(textBoxNazwisko))
             {
-
+                uzytkownik = textBoxImie.Text + " " + textBoxNazwisko.Text + " " + numericUpDownWiek.Value;
+                DodajUzytkownikaDoListy(uzytkownik);
+                uzytkownik = "";
+                
             }
         }
     }
